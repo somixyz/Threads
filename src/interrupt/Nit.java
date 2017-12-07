6ee1a8d8e28a60a9e2a992f60ab7e6dbb06e5f00
Milos Dragovic <milosh.dr93@yahoo.com>
2017-12-07 02:22:44 +0100

    add Nit inside interrupt package

---
+++ src/interrupt/Nit.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interrupt;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Milos Dragovic
+ */
+public class Nit implements Runnable {
+
+    String ime;
+    Random random;
+
+    public Nit(String ime) {
+        this.ime = ime;
+        random = new Random();
+    }
+
+    @Override
+    public void run() {
+        try {
+            //
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Thread ID: " + Thread.currentThread().getId() + " - " +ime );
+                int pauza = random.nextInt(5) * 1000;
+                Thread.sleep(pauza);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Thread A = new Thread(new Nit("A"));
+        A.start();
+        Thread B = new Thread(new Nit("B"));
+        B.start();
+        Thread C = new Thread(new Nit("C"));
+        C.start();
+        Thread D = new Thread(new Nit("D"));
+        D.start();
+        Thread E = new Thread(new Nit("E"));
+        E.start();
+        Thread.sleep(3000); // uspavaj glavnu nit 3s
+        C.interrupt();
+        System.out.print("|C|"); // prekini nit "C"
+        Thread.sleep(3000); // uspavaj glavnu nit 3s
+        D.interrupt();
+        System.out.print("|D|"); // prekini nit "D"
+        Thread.sleep(3000); // uspavaj glavnu nit 3s
+        E.interrupt();
+        System.out.print("|E|"); // prekini nit "E"
+
+    }
+}
