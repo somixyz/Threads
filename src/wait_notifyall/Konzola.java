339f8a8b1c1211d47919642ab69c801a5bf654bb
Milos Dragovic <milosh.dr93@yahoo.com>
2017-12-09 03:52:50 +0100

    dodate neke informacije

--- src/wait_notifyall/Konzola.java
+++ src/wait_notifyall/Konzola.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *  Wait i notify i
+ * Mehanizmi za kontrolu toka izvrsavanja razlicitih niti, preko boolean flag-ova
+ * 
  */
 package wait_notifyall;
 
@@ -72,6 +72,43 @@ class Zdravo extends Thread {
 
 class Test {
 
+    /**
+     * Ključna reč synchronized ne garantuje koliko dugo jedna nit radi sa
+     * jednim resursom. Slededi primer to pokazuje. Treba napraviti dve niti,
+     * koje ce po 50 puta naizmenično da ispisuju Cao! pa Zdravo! u konzoli. Iz
+     * main metode se pokredu dve niti. Obe niti koriste metode objekta klase
+     * Konzola. Te metode su sinhronizovane, što znači da su resursi u njoj
+     * zaključani za korišcenje drugih niti, dok tekuca ne završi posao. Kako
+     * procesor daje određeno vreme jednoj niti, pa drugoj, može se desiti da se
+     * izvrši više od jedne iteracije metode run() i na taj način ispiše više
+     * puta Cao! pre jednog Zdravo! i obrnuto. Mi želelimo da se ispiše jedno
+     * Cao!, zatim jedno Zdravo!, pa opet Cao! itd. Zbog toga, jednu nit treba
+     * staviti na čekanje, dok je druga ne obavesti da može da počne sa radom.
+     *
+     *
+     * Metoda za obaveštanje drugih niti je <b>notify() ili notifyAll()</b>. U ovom
+     * primeru smo koristili notifyAll(). Ukoliko postoji više niti, preporuka
+     * je koristiti notifyAll(), jer se na taj način obaveštavaju sve niti, dok
+     * se metodom notify() nasumično bira nit koja ce prekinuti čekanje.
+     * Čekanje niti na poziv notify() se ostvaruje metodom wait(). Ova metoda baca
+     * InterruptedException ukoliko se nad njom pozove metode interrupt(), a nit
+     * se nalazi u fazi wait(). 
+     * 
+     * Sve metode se pozivaju samo iz syncronized
+     * metoda. Tako su resursi zaključani, ukoliko su zajednički. U našem
+     * slučaju, zajednički resurs je konzola po kojoj pišemo. <b>Redosled kojim ce
+     * se niti izvršavati moramo mi da odredimo. To se radi uz pomoc
+     * booleana/flagova ili sličnih mehanizama.</b> U ovom primeru, koristimo
+     * boolean reciZdravo, koji je na početku postavljen na false, znači prvo ce
+     * se ispisati Cao!. Iz main metode smo startovali obe niti. I jedna, i
+     * druga, pokušavaju da ispišu svoj tekst u konzoli. Zbog booleana
+     * reciZdravo, uslov u niti Zdravo je ispunjen i ta nit je pozvala metodu
+     * wait(). Za to vreme, ispisalo se Cao, zatim je promenjena vrednost
+     * reciZdravo i pozvana je metoda notifyAll(). To je obavestenje za metodu
+     * Zdravo da nastavi sa radom, dok je nit Cao otišla na čekanje (zbog
+     * vrednosti booleana). Sada nit Cao čeka da neko pozove notifyAll(). I sve
+     * se dešava tako u krug dok se ne završi 50-ta iteracija.
+     */
     public static void main(String[] args) {
         Konzola konzola = new Konzola();
         new Cao(konzola).start();
--- src/wait_notifyall_3andmore/Konzola.java
+++ src/wait_notifyall_3andmore/Konzola.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Wait i notify i
+ * Mehanizmi za kontrolu toka izvrsavanja razlicitih niti, preko boolean flag-ova
  */
 package wait_notifyall_3andmore;
 
@@ -11,17 +10,21 @@ package wait_notifyall_3andmore;
  */
 public class Konzola {
 
-    boolean isCao, isZdravo, isDobarDan; 
+    boolean isCao, isZdravo, isDobarDan;
+
     public Konzola() {
         isCao = true;
         isZdravo = false;
         isDobarDan = false;
-    } 
+    }
+
     public synchronized void cao() {
         try {
-            //if
-            while (isCao == false) 
-                wait(); 
+//            if (isDobarDan == false)
+//                wait();
+            while (isCao == false) {
+                wait();
+            }
             System.out.println("CAO!");
             isCao = false;
             isZdravo = true;
@@ -34,8 +37,9 @@ public class Konzola {
     public synchronized void zdravo() {
         try {
             //if
-            while (isZdravo == false) 
-                wait(); 
+            while (isZdravo == false) {
+                wait();
+            }
             System.out.println("\t\t ZDRAVO!");
             isCao = false;
             isZdravo = false;
@@ -48,8 +52,9 @@ public class Konzola {
     public synchronized void dobarDan() {
         try {
             //if
-            while (isDobarDan == false) 
-                wait(); 
+            while (isDobarDan == false) {
+                wait();
+            }
             System.out.println("\t\t\t\t DOBAR DAN!");
             isCao = true;
             isZdravo = false;
@@ -63,9 +68,11 @@ public class Konzola {
 class Cao extends Thread {
 
     Konzola konzola;
+
     public Cao(Konzola konzola) {
         this.konzola = konzola;
-    } 
+    }
+
     @Override
     public void run() {
         for (int i = 0; i < 50; i++) {
@@ -77,9 +84,11 @@ class Cao extends Thread {
 class Zdravo extends Thread {
 
     Konzola konzola;
+
     public Zdravo(Konzola konzola) {
         this.konzola = konzola;
-    } 
+    }
+
     @Override
     public void run() {
         for (int i = 0; i < 50; i++) {
@@ -91,9 +100,11 @@ class Zdravo extends Thread {
 class DobarDan extends Thread {
 
     Konzola konzola;
+
     public DobarDan(Konzola konzola) {
         this.konzola = konzola;
-    } 
+    }
+
     @Override
     public void run() {
         for (int i = 0; i < 50; i++) {
@@ -102,11 +113,43 @@ class DobarDan extends Thread {
     }
 }
 
-class Test { 
+class Test {
+
+    /**
+     * Pored dve niti koje ispisuju Cao! i Zdravo!, ovde dodajemo i tredu nit
+     * koja ispisuje DobarDan!. Ideja je ista, redosled ispisivanja da bude Cao!
+     * Zdravo! Dobar dan! Cao! Zdravo!
+     *
+     * Sada nije dovoljna boolean vrednost da proveri sva tri slučaja, pa su
+     * uvedene 3 vrednosti. Kada je neka od njih true, to je slededa nit koja
+     * treba da se izvrši. Kada se ispiše Cao!, boolean za nit Zdravo se postavi
+     * na true, ostale na false i ista logika je za druge niti.
+     *
+     * Sada nije dovoljna boolean vrednost da proveri sva tri slučaja, pa su
+     * uvedene 3 vrednosti. Kada je neka od njih true, to je slededa nit koja
+     * treba da se izvrši. Kada se ispiše Cao!, boolean za nit Zdravo se postavi
+     * na true, ostale na false i ista logika je za druge niti.
+     *
+     * Rezultat nije očekivan, ispiše se Cao! , Zdravo!, pa opet Cao! itd. To je
+     * zbog toga što se provera za čekanje vrši if uslovom. Ono što se desi je
+     * da se ispiše Cao!, postave booleani da se sledede ispiše Zdravo!. Nakon
+     * Zdravo! poziva se notifyAll(). Za to vreme i Cao i DobarDan su ušle u if
+     * uslov i čekaju da se prekine metoda wait(). Kada to uradi Zdravo, opet se
+     * ispiše Cao.
+     * <b>Ono što je dobra praksa i što bi uvek trebalo raditi, je da se if
+     * uslov zameni while uslovom. 2 Sve tri metode bi trebao da liče na
+     * slededu.</b>
+     *
+     * Sada, kada se ispiše Cao!, nameste booleani, ispiše Zdravo!, ponovo
+     * nameste booleani i pozove metoda notifyAll(), obavesti se i nit Cao da
+     * izađe iz wait() stanja. Međutim, kako se sad taj uslov nalazi u petji,
+     * opet se proveri da li je zaista nit Cao ta koja treba da nastavi sa
+     * izvršavanjem. Ako nije, ona opet ulazi u fazu wait()
+     */
     public static void main(String[] args) {
         Konzola konzola = new Konzola();
         new Cao(konzola).start();
         new Zdravo(konzola).start();
         new DobarDan(konzola).start();
     }
-}  
+}
